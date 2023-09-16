dd797a6ca02c324ac1bf034ae5106c17ec132421
Farhad Shamsi <farhadshamsi2200@gmail.com>
2023-09-16 19:35:51 +0330

    Debug Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -10,10 +10,10 @@ const Cart = ({data}) => {
             <div>
                 <span className={styles.country}>{data.name} </span>
                 <span className={styles.land}>{data.sys.country}</span>
-            </div>\2.14
+            </div>
             <h3 className={styles.temp}>{Math.round(data.main.temp)} C</h3>
             <figure className={styles.figure}>
-                <img src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`} alt="           ." />
+                <img src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`} alt="." />
                  <figcaption>{data.weather[0].description}</figcaption>
             </figure>
         </div>
